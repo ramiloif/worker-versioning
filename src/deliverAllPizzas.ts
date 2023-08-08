95c6bcda2be983171f869041194d50e2fa474865
ramiloif <ramiloif@gmail.com>
2023-08-08 18:14:11 +0300

    finish

--- src/deliverAllPizzas.ts
+++ src/deliverAllPizzas.ts
@@ -1,6 +1,10 @@
 import { Client, Connection, WorkflowClient } from '@temporalio/client';
 import { taskQueue } from './constants';
 
+/**
+ * 
+ * This code is general for all workflows not aware of the versioning
+ */
 const run = async () => {
 
   const connection = await Connection.connect();
@@ -16,23 +20,6 @@ const run = async () => {
     const handle = client.getHandle(e.execution!.workflowId!);
     await handle.signal('deliverOrder', '');
   });
-
-  const reachability = await new Client().taskQueue.getReachability({
-    taskQueues: [taskQueue],
-    buildIds: ['1.0'],
-  });
-
-  console.log('Reachability:', JSON.stringify(reachability));
-  const taskQueueReachability = reachability.buildIdReachability['1.0'].taskQueueReachability[taskQueue];
-  for (const [taskQueue, reachability] of Object.entries(taskQueueReachability)) {
-    if (reachability.length > 0) {
-      if (reachability[0] === 'NotFetched') {
-        console.log("rhat?")
-      } else {
-        console.log('Build id still reachable on:', taskQueue);
-      }
-    }
-};
 };
 
 run().catch((err) => {
